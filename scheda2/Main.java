e7b5b9cbabe5a55d41a5b59d2b2350a93e721ad9
CristianTorrisi <cristiantorrisi31.ct@gmail.com>
2023-10-07 11:52:06 +0200

    Terminata la scheda 2 del modulo 1

--- scheda2/Main.java
+++ scheda2/Main.java
@@ -14,6 +14,20 @@ public class Main {
     //    System.out.println(Esercizio4.fattoriale(3));
      //   System.out.println(Esercizio4.fattoriale2(3));
         System.out.println(" ------------- ");
+     //   double armonica_somma = Esercizio5.armonica(2);
+      //  System.out.println(armonica_somma);
+      //  double armonica_somma_recursive = Esercizio5.armonicaRecursive(2);
+      //  System.out.println(armonica_somma_recursive);
+        System.out.println(" ------------- ");
+     //   boolean palindromo = Esercizio6.isPalindroma("I topi non avevano nipoti");
+     //   boolean palindromo2 = Esercizio6.isPalindroma("CECCO");
+      //  boolean palindromo3 = Esercizio6.isPalindroma("Anna");
+      //  System.out.println(palindromo);
+      //  System.out.println(palindromo2);
+      //  System.out.println(palindromo3);
+
+
+
 
 
 
