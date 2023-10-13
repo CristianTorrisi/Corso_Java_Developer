5d52c7d1e4e7e1f4cc3c2b62bdad53e0e644e712
CristianTorrisi <cristiantorrisi31.ct@gmail.com>
2023-10-13 11:38:26 +0200

    Ho completo la lista di interi

--- modulo4/lista_interi/ListaDiInteri.java
+++ modulo4/lista_interi/ListaDiInteri.java
@@ -66,6 +66,64 @@ public class ListaDiInteri {
         array[array.length-1] = i;
     }
 
+    /**
+     * Inserisco un elemento in una precisa posizione nella lista
+     * @param element elemento da inserire
+     * @param i posizione dove inserire l'elemento
+     */
+    public void addAt(int element,int i){
+        int[] tmp = new int[array.length+1];
+
+        tmp = copyArray(tmp,array,i);
+        tmp[i] = element;
+        for (int j = i+1; j < array.length+1; j++) {
+            tmp[j] = array[j-1];
+        }
+
+        array = tmp;
+    }
+
+    /**
+     * Rimuovi la prima occorrenza di un elemento dato
+     * @param element elemento da cancellare
+     */
+    public void remove(int element){
+        int index = indexOf(element);
+        if (index == -1) return;
+        pop(index);
+    }
+
+    /**
+     * Funzione che elemina l'occorenza di un elmemento dato un indice(funzione parte da remove())
+     * @param index indice di elemento da eliminare
+     */
+    public void pop(int index){
+        if (length()<index) return;
+
+        int[] tmp = new int[array.length-1];
+
+        tmp = copyArray(tmp,array,index);
+        for (int i = index; i < array.length-1; i++) {
+            tmp[i] = array[i+1];
+        }
+
+        array = tmp;
+    }
+
+    /**
+     * Copia una parte di array fino ad un indicie dato
+     * @param tmp array dove copiare
+     * @param array array da copiare
+     * @param i indice fino a dove mi devo fermare per la copia
+     * @return array copiato
+     */
+    public int[] copyArray(int[] tmp,int[] array, int i){
+        for (int j = 0; j < i; j++) {
+            tmp[j] = array[j];
+        }
+        return tmp;
+    }
+
     public static void main(String[] args) {
                 ListaDiInteri lista = new ListaDiInteri();
                 lista.add(1);
@@ -76,6 +134,12 @@ public class ListaDiInteri {
                 System.out.println(lista.contains(3));
                 System.out.println(lista.getI(2));
                 System.out.println(lista.indexOf(3));
+                System.out.println(lista.toString());
+                lista.addAt(10,1);
+                System.out.println(lista.toString());
+                lista.remove(10);
+                System.out.println(lista.toString());
+
             }
 
 }
